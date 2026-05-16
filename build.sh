f999ed77aa2eff7bd886bed47e16f376f98902ff
lsyes <3294271542@qq.com>
2026-05-16 22:53:59 +0800

    fix the path error

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 git clone --depth 1 https://github.com/loongson/jdk25u.git ../jdk25u
 mv debian ../jdk25u/
 cd ../jdk25u
-git clone --depth 1 https://github.com/google/googletest.git test/googletest
+git clone --depth 1 https://github.com/google/googletest.git googletest
 sudo apt update
 sudo apt build-dep -y .
 dpkg-buildpackage -b -us -uc -d
