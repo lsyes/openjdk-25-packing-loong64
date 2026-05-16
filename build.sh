300a67b4e40a99d032078b5ee4f201356186ed06
lsyes <3294271542@qq.com>
2026-05-16 22:38:21 +0800

    Refactor build script for OpenJDK and dependencies

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-git clone --depth 1 https://github.com/loongson/jdk25u.git
-cd jdk25u
+cd openjdk-25-packing-loong64
 
-git clone https://github.com/lsyes/openjdk-25-packing-loong64.git /tmp/openjdk-25-packing
+git clone --depth 1 https://github.com/loongson/jdk25u.git ../jdk25u
 
-mv /tmp/openjdk-25-packing/debian .
+mv debian ../jdk25u/
 
-rm -rf /tmp/openjdk-25-packing
+cd ../jdk25u
 
 git clone --depth 1 https://github.com/google/googletest.git test/googletest
 
 sudo apt update && sudo apt build-dep -y .
+
 # 构建
 dpkg-buildpackage -b -us -uc -d
