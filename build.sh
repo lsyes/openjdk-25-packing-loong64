a1ef490487155ec09b1dded141f428761ebef5ba
lsyes <3294271542@qq.com>
2026-05-16 22:45:35 +0800

    Refactor build.sh to simplify commands

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 git clone --depth 1 https://github.com/loongson/jdk25u.git ../jdk25u
-
 mv debian ../jdk25u/
-
 cd ../jdk25u
-
 git clone --depth 1 https://github.com/google/googletest.git test/googletest
-
-sudo apt update && sudo apt build-dep -y .
-
-# 构建
+sudo apt update
+sudo apt build-dep -y .
 dpkg-buildpackage -b -us -uc -d
